d1fd91e790869d9f930d30cbfba25b11a748ae60
Octal1021 <Hexidecimal21@outlook.com>
2024-01-21 22:39:10 -0700

    css almost fixed, need to update some image and colors then, mobile css file

--- shippingbyco/src/pages/Main.js
+++ shippingbyco/src/pages/Main.js
@@ -1,13 +1,20 @@
 import React, {useEffect, useState, useRef} from "react";
 import { motion } from "framer-motion";
 import './style.css';
+import './mobile-css.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faEnvelope, faMailForward, faMinus, faPhone, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faLinkedin, faTiktok, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 const Main = () => {
 
     const [contact1Expanded, setContact1Expanded] = useState(false)
     const [contact2Expanded, setContact2Expanded] = useState(false)
+    const [phoneRevealed1, setPhoneRevealed1] = useState(false)
+    const [phoneRevealed2, setPhoneRevealed2] = useState(false)
     const [titleText, setTitleText] = useState('Shipment By Colorado');
-    const [animationClass, setAnimationClass] = useState('fadeOut');
+    const [animationclassName, setAnimationclassName] = useState('fadeOut');
     const contactRef = useRef(null);
 
     const scrollToContact = () => {
@@ -15,11 +22,10 @@ const Main = () => {
     }
 
     useEffect(() => {
-        // Change the text after the fade-out animation
         const timer = setTimeout(() => {
             setTitleText('SBC');
-            setAnimationClass('fadeIn');
-        }, 1500); 
+            setAnimationclassName('fadeIn');
+        }, 2000); 
 
         return () => clearTimeout(timer);
     }, []);
@@ -27,44 +33,120 @@ const Main = () => {
     return (
     <div className="container">
         <div className="header-banner">
-            <img src='/logo512.png' className="main-logo-img"/>
-            <h1 className={`dynamic-title ${animationClass}`}>{titleText}</h1>
+            <img src='/logo192.png' id="main-logo-img"/>
+            <div className='separator'></div>
+            <h1 className={`dynamic-title ${animationclassName}`}>{titleText}</h1>
         </div>
         <div className="main-slogan-catch-container">
             <h1>Beyond Service, Beyond Satisfaction - Experience Business Excellence!</h1>
             <button className='functional-header-btn' onClick={scrollToContact}><h2>Contact Us</h2></button>
         </div>
         <div className="about-sect">
-        <h1>About Us</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus ultricies tristique nulla aliquet enim. Nam libero justo laoreet sit amet. Felis donec et odio pellentesque diam. Etiam non quam lacus suspendisse faucibus interdum. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Leo a diam sollicitudin tempor id eu. Diam sit amet nisl suscipit. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Integer feugiat scelerisque varius morbi enim nunc faucibus. Risus pretium quam vulputate dignissim. Tellus mauris a diam maecenas sed enim ut sem. Id donec ultrices tincidunt arcu. Velit scelerisque in dictum non.
-            Eget nunc scelerisque viverra mauris in aliquam sem. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Faucibus interdum posuere lorem ipsum dolor. Tempor orci dapibus ultrices in. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Lectus proin nibh nisl condimentum. Ac turpis egestas maecenas pharetra convallis posuere. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Proin sed libero enim sed faucibus turpis in eu. Vulputate ut pharetra sit amet aliquam id diam maecenas. Sed viverra ipsum nunc aliquet bibendum enim facilisis. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Quis hendrerit dolor magna eget. Leo a diam sollicitudin tempor id eu nisl nunc mi. Duis at tellus at urna condimentum mattis pellentesque.
-            Sit amet luctus venenatis lectus magna fringilla urna porttitor. Amet risus nullam eget felis. Facilisis volutpat est velit egestas dui id ornare arcu odio. Varius duis at consectetur lorem donec massa sapien. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Suscipit tellus mauris a diam maecenas sed enim ut. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Amet mauris commodo quis imperdiet massa tincidunt nunc. Imperdiet massa tincidunt nunc pulvinar sapien et ligula. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Tellus integer feugiat scelerisque varius morbi enim. Vel orci porta non pulvinar neque laoreet suspendisse interdum. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Id semper risus in hendrerit gravida. Id venenatis a condimentum vitae sapien pellentesque habitant morbi.
-            Eu scelerisque felis imperdiet proin fermentum. Gravida arcu ac tortor dignissim convallis aenean et tortor at. Nisl rhoncus mattis rhoncus urna neque viverra. Ut aliquam purus sit amet. Est pellentesque elit ullamcorper dignissim cras. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Ultrices in iaculis nunc sed augue lacus viverra. Lectus proin nibh nisl condimentum id venenatis a condimentum. Venenatis a condimentum vitae sapien. Ut ornare lectus sit amet est. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Et molestie ac feugiat sed lectus vestibulum. Aliquam ut porttitor leo a diam sollicitudin tempor. Augue ut lectus arcu bibendum at varius vel pharetra.
-            Volutpat ac tincidunt vitae semper quis lectus. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Facilisis volutpat est velit egestas dui id ornare. Turpis egestas sed tempus urna et pharetra pharetra. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis. Fermentum dui faucibus in ornare quam viverra. Massa tempor nec feugiat nisl pretium fusce id velit. Tincidunt augue interdum velit euismod in pellentesque. A arcu cursus vitae congue mauris rhoncus aenean vel. Sit amet nisl suscipit adipiscing bibendum est ultricies. Eu volutpat odio facilisis mauris sit amet massa. Ultrices dui sapien eget mi proin sed libero enim. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Purus viverra accumsan in nisl nisi scelerisque eu. Sed risus pretium quam vulputate dignissim suspendisse. Et magnis dis parturient montes nascetur. Volutpat diam ut venenatis tellus in metus. Turpis cursus in hac habitasse platea dictumst. Gravida rutrum quisque non tellus orci ac auctor augue mauris.
+            <p id="paragraph-top">
+                Beyond buying and selling properties, we're committed to providing helpful services that go the extra mile. Whether it's expert advice on the market trends, personalized consultations, or resources to make informed decisions, we're here to empower you on whatever endeavors you might have. Join us in turning dreams into addresses and discover a world where motivation meets exceptional service.
             </p>
+            <img id='about-photo-main' src='./about-photo-main.png'/>
 
-            <div className={contact1Expanded ? 'contact-card-expanded':'contact-card'} onClick={() => setContact1Expanded(!contact1Expanded)}>
-                <h2> Van</h2>
-            </div>
-            <div className={contact2Expanded ? 'contact-card-expanded':'contact-card'} onClick={() => setContact2Expanded(!contact2Expanded)}>
-                <h2></h2>
-            </div>
-        </div>
-        <div className="services-sect">
-        <h1>Services</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus ultricies tristique nulla aliquet enim. Nam libero justo laoreet sit amet. Felis donec et odio pellentesque diam. Etiam non quam lacus suspendisse faucibus interdum. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Leo a diam sollicitudin tempor id eu. Diam sit amet nisl suscipit. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Integer feugiat scelerisque varius morbi enim nunc faucibus. Risus pretium quam vulputate dignissim. Tellus mauris a diam maecenas sed enim ut sem. Id donec ultrices tincidunt arcu. Velit scelerisque in dictum non.
-            Eget nunc scelerisque viverra mauris in aliquam sem. Massa eget egestas purus viverra accumsan in nisl nisi scelerisque. Faucibus interdum posuere lorem ipsum dolor. Tempor orci dapibus ultrices in. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Lectus proin nibh nisl condimentum. Ac turpis egestas maecenas pharetra convallis posuere. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Proin sed libero enim sed faucibus turpis in eu. Vulputate ut pharetra sit amet aliquam id diam maecenas. Sed viverra ipsum nunc aliquet bibendum enim facilisis. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Quis hendrerit dolor magna eget. Leo a diam sollicitudin tempor id eu nisl nunc mi. Duis at tellus at urna condimentum mattis pellentesque.
-            Sit amet luctus venenatis lectus magna fringilla urna porttitor. Amet risus nullam eget felis. Facilisis volutpat est velit egestas dui id ornare arcu odio. Varius duis at consectetur lorem donec massa sapien. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Suscipit tellus mauris a diam maecenas sed enim ut. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Amet mauris commodo quis imperdiet massa tincidunt nunc. Imperdiet massa tincidunt nunc pulvinar sapien et ligula. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Tellus integer feugiat scelerisque varius morbi enim. Vel orci porta non pulvinar neque laoreet suspendisse interdum. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Id semper risus in hendrerit gravida. Id venenatis a condimentum vitae sapien pellentesque habitant morbi.
-            Eu scelerisque felis imperdiet proin fermentum. Gravida arcu ac tortor dignissim convallis aenean et tortor at. Nisl rhoncus mattis rhoncus urna neque viverra. Ut aliquam purus sit amet. Est pellentesque elit ullamcorper dignissim cras. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Ultrices in iaculis nunc sed augue lacus viverra. Lectus proin nibh nisl condimentum id venenatis a condimentum. Venenatis a condimentum vitae sapien. Ut ornare lectus sit amet est. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Et molestie ac feugiat sed lectus vestibulum. Aliquam ut porttitor leo a diam sollicitudin tempor. Augue ut lectus arcu bibendum at varius vel pharetra.
-            Volutpat ac tincidunt vitae semper quis lectus. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Facilisis volutpat est velit egestas dui id ornare. Turpis egestas sed tempus urna et pharetra pharetra. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis. Fermentum dui faucibus in ornare quam viverra. Massa tempor nec feugiat nisl pretium fusce id velit. Tincidunt augue interdum velit euismod in pellentesque. A arcu cursus vitae congue mauris rhoncus aenean vel. Sit amet nisl suscipit adipiscing bibendum est ultricies. Eu volutpat odio facilisis mauris sit amet massa. Ultrices dui sapien eget mi proin sed libero enim. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Purus viverra accumsan in nisl nisi scelerisque eu. Sed risus pretium quam vulputate dignissim suspendisse. Et magnis dis parturient montes nascetur. Volutpat diam ut venenatis tellus in metus. Turpis cursus in hac habitasse platea dictumst. Gravida rutrum quisque non tellus orci ac auctor augue mauris.
+            <p id="paragraph-bottom">
             </p>
+            <div className="personal-contact-sect">
+                <h1>Personal Contact</h1>
+                <div className="contact-cards">
+                    <div className={contact1Expanded ? 'contact-card-expanded':'contact-card'} onClick={() => setContact1Expanded(!contact1Expanded)}>
+                        <div className="title-bar">
+                            <img className={contact1Expanded ? 'headshot-expanded':"headshot"} src="/headshot-van.png"/>
+                            <FontAwesomeIcon className={contact1Expanded ?  'plus-minus-expanded' : 'plus-minus'} icon={contact1Expanded ?  faMinus : faPlus} />
+                            <h2 className="name">Van Cessford</h2>
+                        </div>
+                        {contact1Expanded &&
+                            <div className="card-content"> 
+                                <h3 className="contact-card-about">Hi! My name is Van I am based out of Denver Colorado. I am a young entrepreneur who is driven and passionate about making a positive impact through my services. I have 4 years of sales experience, and I’m always looking to contribute meaningful insight to anyone who may need it! </h3>
+                                <div className="personal-links">   
+                                    <div className="contact-card-link">
+                                            <FontAwesomeIcon className="link-icon-cc" icon={faPhone} />
+                                            <h3>Phone: 303-859-3038</h3>
+                                    </div> 
+                                    <a href="https://www.linkedin.com/in/van-cessford-54a034263/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                                        <div className="contact-card-link">
+                                            <FontAwesomeIcon className="link-icon-cc" icon={faLinkedin} />
+                                            <h3>LinkedIn</h3>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                    <div className={contact2Expanded ? 'contact-card-expanded':'contact-card'} onClick={() => setContact2Expanded(!contact2Expanded)}>
+                        <div className="title-bar">
+                            <img className={contact2Expanded ? 'headshot-expanded':"headshot"} src="/headshot-don.png"/>
+                            <FontAwesomeIcon className={contact2Expanded ?  'plus-minus-expanded' : 'plus-minus'} icon={contact2Expanded ?  faMinus : faPlus} />
+                            <h2 className="name">Donaven Tate</h2>
+                        </div>
+                        {contact2Expanded &&
+                            <div className="card-content"> 
+                                <h3 className="contact-card-about">Hi, My Name is Don. I am based out of Colorado Springs, Colorado. A very motivated entrepreneur with 7+ years in sales experience. Graduate of Metropolitan State University with a Degree in Communications. Always looking for the next opportunity to change not only mine but others lives! </h3>
+                                <div className="personal-links">   
+                                    <div className="contact-card-link" onClick={() => setPhoneRevealed2(true)}>
+                                            <FontAwesomeIcon className="link-icon-cc" icon={faPhone} />
+                                            <h3>Phone: 719-960-8728</h3>
+                                    </div> 
+                                    <a href="https://www.linkedin.com/in/donaven-tate-80a247237/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                                        <div className="contact-card-link">
+                                            <FontAwesomeIcon className="link-icon-cc" icon={faLinkedin} />
+                                            <h3>LinkedIn</h3>
+                                        </div>
+                                    </a>
+                                </div>
+                           </div>
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
         <div ref={contactRef} className="contact-sect">
             <h1>Contact Us</h1>
-            <h2>Email:</h2>
-            <h2>Phone Number:</h2>
+                <div className="links">
+                    <div className="ind-link-container">
+                        <a href='https://www.homedepot.com'>
+                            <div className="link-box">
+                                <FontAwesomeIcon className="link-icon" icon={faEnvelope} />
+                                <h2>Email</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ind-link-container">
+                        <a href='https://www.homedepot.com'>
+                            <div className="link-box">
+                                <FontAwesomeIcon className="link-icon" icon={faPhone} />
+                                <h2>Phone</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ind-link-container">
+                        <a href='https://www.facebook.com/profile.php?id=61550938487372&mibextid=hu50Ix'>
+                            <div className="link-box">
+                                <FontAwesomeIcon className="link-icon" icon={faFacebook} />
+                                <h2>Facebook</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ind-link-container">
+                        <a href='https://www.instagram.com/shipmentbycolorado'>
+                            <div className="link-box">
+                                <FontAwesomeIcon className="link-icon" icon={faInstagram} />
+                                <h2>Instagram</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ind-link-container">
+                        <a href='https://www.homedepot.com'>
+                            <div className="link-box">
+                                <FontAwesomeIcon className="link-icon" icon={faLinkedin} />
+                                <h2>LinkedIn</h2>
+                            </div>
+                        </a>
+                    </div>
+                </div>
         </div>
     </div>
     );
